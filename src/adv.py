f4972d435d7d2a8e0b5b6b42616e254d104b58af
RobertMisch <robertmisch94@gmail.com>
2020-06-03 18:49:55 -0400

    got a really late start

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
-
+from player import Player
+import sys
 # Declare all the rooms
 
 room = {
@@ -33,19 +34,98 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
+#[OV] [TR]
+# |     |
+#[FO]-[NA]
+# |
+#[OT]
+
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
+newPlayer= Player('myname','outside',['item1','item2'])
+print(newPlayer)
 
 # Write a loop that:
+running=True
+while(running):
 #
 # * Prints the current room name
+    current_location=newPlayer.get_location
+    print(current_location)
 # * Prints the current description (the textwrap module might be useful here).
+    print(room[f"{current_location}"].get_description)
 # * Waits for user input and decides what to do.
+    input = input("what action would you like to take? : ")
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
+    if(input=="q"):
+        print("Goodbye!")
+        sys.exit(1)
 # If the user enters "q", quit the game.
+
+
+
+
+#stuff from class
+# #define a store using OOP principals
+
+# class Store:
+#     def __init__(self, name, departments):
+#         self.name=name
+#         #departments will be a list strings that we need to make departments
+#         self.departments= self.init_departments(departments)
+
+#     def __str__(self):
+#         #will print out the name of the store
+#         #as well as any departments that the store has (stores can have more than 1 dept)
+#         output= f"{self.name} \n"
+#         for dept in self.departments:
+#             output += "  id: " + str(dept.get_id()) + ", name: " + dept.get_name() + "\n"
+#         return output
+#     def init_departments(self, departments):
+#         #normal version
+#         # instances = []
+#         # for index, dept in enumerate(departments):
+#         #     instances.append(Department(index+1, dept))
+#         # return instances
+#         #list comprehension
+#         return [Department(index+1 , dept) for index, dept in enumerate(departments)]
+
+# #not having it inherit since it didn't make sense to our instructor, make sure to think through if something is distinct
+# class Department:
+#     def __init__(self, id, name):
+#         self.id=id
+#         self.name=name
+#     def __str__(self):
+#         return f"Department {self.id}: {self.name}"
+#     def get_id(self):
+#         return self.id
+
+#     def get_name(self):
+#         return self.name
+
+# departments=[
+#     "running",
+#     "fishing",
+#     "baseball",
+#     "basketball"
+#     ]
+# # Department(4, "basketball") used to have the departments structured like this
+# my_store = Store("The Dugout", departments)
+# print(my_store)
+
+# selection = input("select the department number: ")
+# print(f"you selected Department {selection}, {my_store.departments[int(selection)-1].get_name()}")
+
+#a couple things that we can think about fixing
+#there's no way to access departments easily outside of our store class
+#fixed sorta, but we need to not pull from the input array as the source of truth
+#fixed to taking just from my_store and callind get name for the dept
+
+#streamline adding departments to our store
+#adding a method on the store class that will taking a list of strings and make them departments
--- src/player.py
+++ src/player.py
@@ -1,2 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, location, items):
+        self.name=name
+        self.location=location
+        self.items=items
+    def __str__(self):
+        inventory=""
+        for item in self.items:
+            inventory= inventory + " " + item + "|"
+        return f"name: {self.name}, location: {self.location}, items:{inventory}"
+    def get_location(self):
+        return f"{self.location}"
